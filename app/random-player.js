df578cc92416f7fb1f0eb019a4f64184a46b8593
Bradley J. Spaulding <brad.spaulding@gmail.com>
2014-12-23 20:31:11 -0500

    Fixed jslint errors in RandomPlayer

--- app/random-player.js
+++ app/random-player.js
@@ -1,6 +1,22 @@
-var RandomPlayer = (function(_super) {
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+/*global Suit: false, Player: false */
+var RandomPlayer = (function (_super) {
+  "use strict";
+
+  var __extends = function (child, parent) {
+      var key;
+      for (key in parent) {
+        if (parent.hasOwnProperty(key)) {
+          child[key] = parent[key];
+        }
+      }
+      function Ctor() {
+        this.constructor = child;
+      }
+      Ctor.prototype = parent.prototype;
+      child.prototype = new Ctor();
+      child.__super__ = parent.prototype;
+      return child;
+    };
 
   __extends(RandomPlayer, _super);
 
@@ -8,21 +24,21 @@ var RandomPlayer = (function(_super) {
     return RandomPlayer.__super__.constructor.apply(this, arguments);
   }
 
-  RandomPlayer.prototype.bid = function() {
-    return parseInt(Math.random() * (63-15), 10) + 15;
-  }
+  RandomPlayer.prototype.bid = function () {
+    return parseInt(Math.random() * (63 - 15), 10) + 15;
+  };
 
-  RandomPlayer.prototype.declareTrump = function() {
+  RandomPlayer.prototype.declareTrump = function () {
     return Suit.suits[parseInt(Math.random() * 4, 10)];
-  }
+  };
 
-  RandomPlayer.prototype.nextCardToPlay = function() {
+  RandomPlayer.prototype.nextCardToPlay = function () {
     return this.cards[0];
-  }
+  };
 
-  RandomPlayer.prototype.cardsToDiscard = function() {
+  RandomPlayer.prototype.cardsToDiscard = function () {
     return this.cards.slice(6);
-  }
+  };
 
   return RandomPlayer;
 }(Player));
