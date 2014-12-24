9f7e181beda0d7157f69fe4ac85831ce2660f40a
Bradley J. Spaulding <brad.spaulding@gmail.com>
2014-12-23 19:55:14 -0500

    Fixed jslint errors in SixtyThree

--- app/sixty-three.js
+++ app/sixty-three.js
@@ -1,38 +1,42 @@
-var SixtyThree = (function() {
+/*global Deck: false, RuleSet: false, Round: false, RandomPlayer: false, _: false */
+var SixtyThree = (function () {
+  "use strict";
+
   function SixtyThree() {
     this.deck = new Deck();
     this.ruleSet = new RuleSet();
   }
 
-  SixtyThree.prototype.start = function() {
+  SixtyThree.prototype.start = function () {
     this.setupPlayers();
 
-    while ( !this.gameIsOver() ) {
-      var round = new Round(this);
+    var round;
+    while (!this.gameIsOver()) {
+      round = new Round(this);
       round.play();
     }
-  }
+  };
 
-  SixtyThree.prototype.setupPlayers = function() {
+  SixtyThree.prototype.setupPlayers = function () {
     this.players = [
       new RandomPlayer("Player 1"),
       new RandomPlayer("Player 2"),
       new RandomPlayer("Player 3"),
       new RandomPlayer("Player 4")
     ];
-  }
+  };
 
-  SixtyThree.prototype.gameIsOver = function() {
+  SixtyThree.prototype.gameIsOver = function () {
     return this.topScore() >= 200;
-  }
+  };
 
-  SixtyThree.prototype.topScore = function() {
-    var playersSortedByScoreAsc = _.sortBy(this.players, function(player) {
+  SixtyThree.prototype.topScore = function () {
+    var playersSortedByScoreAsc = _.sortBy(this.players, function (player) {
       return player.score();
-    })
+    });
 
     return _.last(playersSortedByScoreAsc).score();
-  }
+  };
 
   return SixtyThree;
 }());
