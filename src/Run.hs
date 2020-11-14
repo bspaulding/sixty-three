75e33f6a94a38f19186fbe463823fddb99c0fa7f
Bradley J. Spaulding <brad.spaulding@gmail.com>
2020-11-14 14:46:05 -0800

    shuffle is pure

--- src/Run.hs
+++ src/Run.hs
@@ -6,10 +6,12 @@ module Run (run) where
 import Import
 import Shuffle (shuffle)
 import SixtyThree
+import System.Random (getStdGen)
 
 run :: RIO App ()
 run = do
   logInfo "We're inside the application!"
   let (Cards cards) = deck
-  shuffled <- liftIO $ shuffle cards
+  g <- liftIO getStdGen
+  let (shuffled, _) = shuffle cards g
   logInfo $ display (Cards shuffled)
--- src/Shuffle.hs
+++ src/Shuffle.hs
@@ -1,17 +1,18 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Shuffle (shuffle) where
 
 import Import
-import Prelude (splitAt)
 import System.Random
+import Prelude (splitAt)
 
-shuffle :: [a] -> IO [a]
+shuffle :: RandomGen g => [a] -> g -> ([a], g)
 shuffle ls = shuffle' ls []
   where
-    shuffle' [] acc = return acc
-    shuffle' l acc = do
-      k <- randomRIO(0, length l - 1)
-      let (lead, x:xs) = splitAt k l
-      shuffle' (lead ++ xs) (x:acc)
+    shuffle' :: RandomGen g => [a] -> [a] -> g -> ([a], g)
+    shuffle' [] acc g = (acc, g)
+    shuffle' l acc g = do
+      let (k, g') = randomR (0, length l - 1) g
+      let (lead, x : xs) = splitAt k l
+      shuffle' (lead ++ xs) (x : acc) g'
