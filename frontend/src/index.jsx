2e38addd90841cccfc869107bf24a83d5ad046a9
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-01-07 10:59:32 -0800

    removed extra test socket

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,12 +2,4 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/App.jsx';
 
-const socket = new WebSocket('ws://localhost:3000');
-socket.addEventListener('message', event => {
-  console.log('got a message: ', event);
-});
-socket.addEventListener('close', event => {
-  console.log('vanilla socket closed!', event);
-});
-
 ReactDOM.render(React.createElement(App), document.getElementById("app"));
