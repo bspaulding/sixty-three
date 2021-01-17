fc9c3d4382774dd956cdf66322653fec9bde3ccf
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-01-16 16:40:30 -0800

    added test for setting player name while in room

--- test/ServerStateSpec.hs
+++ test/ServerStateSpec.hs
@@ -13,10 +13,12 @@ spec = do
     let g = mkStdGen 1
     let initialState = newServerState
     let connId = "abcdefg"
+    let roomId = "abcd"
 
     describe "SetPlayerName" $ do
+      let request = SetPlayerName "Bradley"
+
       it "sets player name sends echo back if no room yet" $ do
-        let request = SetPlayerName "Bradley"
         let result = serverStateReducer g initialState connId request reducerSafe
         let state = fst <$> result
         let msgs = snd <$> result
@@ -24,7 +26,13 @@ spec = do
         msgs `shouldBe` Right [(connId, SocketResponse.PlayerNameChanged connId "Bradley")]
 
       it "sets player name and broadcasts to room, if room" $ do
-        pending
+        let initialState = moveClientToRoom roomId connId $ moveClientToRoom roomId "123" newServerState
+        let result = serverStateReducer g initialState connId request reducerSafe
+        let state = fst <$> result
+        let msgs = snd <$> result
+        playerName connId <$> state `shouldBe` Right "Bradley"
+        let expectedMsg = SocketResponse.PlayerNameChanged connId "Bradley"
+        msgs `shouldBe` Right [(connId, expectedMsg), ("123", expectedMsg)]
 
     describe "JoinRoom" $ do
       it "lower cases all room ids" $ do
