7629b322706915c74b68bf0c55f408ad2238196f
Bradley J. Spaulding <brad.spaulding@gmail.com>
2021-01-26 23:55:32 -0800

    Removed trailing whitespace

--- backend/test/SocketRequestSpec.hs
+++ backend/test/SocketRequestSpec.hs
@@ -2,7 +2,7 @@
 
 module SocketRequestSpec (spec) where
 
-import Data.Aeson 
+import Data.Aeson
 import GameAction
 import Player
 import SocketRequest
@@ -25,6 +25,6 @@ spec = do
     it "can decode (PlayerOne, BidPass)"  $ do
       (decode "{\"type\":\"GameAction\",\"payload\":[\"PlayerOne\", {\"type\":\"BidPass\"}]}" :: Maybe (SocketRequest (Player, GameAction)))
         `shouldBe` Just (GameAction (PlayerOne, BidPass))
-    
+
     it "can encode 2" $ do
       encode (GameAction (PlayerOne, BidPass)) `shouldBe` "{\"payload\":[\"PlayerOne\",{\"type\":\"BidPass\"}],\"type\":\"GameAction\"}"
